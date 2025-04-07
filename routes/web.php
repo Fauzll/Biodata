ebc8502d8b4ff0c8d7f77a17eee586cef9ab51ef
Fauzll <163851720+Fauzll@users.noreply.github.com>
2025-04-07 21:08:27 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
+use App\Http\Controllers\HtmlController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
-use App\Http\Controllers\HtmlController;
+use App\Http\Controllers\LatihanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('helloworld',[HelloWorldController::class, 'index']);
-Route::get('ambilfile',[HelloWorldController::class, 'ambilfile']);
-Route::get('getlorem',[HtmlController::class, 'getLorem']);
+Route::get('/helloworld', [HelloWorldController::class, 'index']);
+Route::get('/ambilfile', [HelloWorldController::class, 'ambilfile']);
+Route::get('/getlorem', [HtmlController::class, 'getlorem']);
+Route::get('/tabel', [LatihanController::class, 'getTabel'])->name('latihan.tabel');
+Route::get('/form', [LatihanController::class, 'getForm'])->name('latihan.form');
+Route::post('/form/simpan', [LatihanController::class, 'simpan'])->name('latihan.simpan');
